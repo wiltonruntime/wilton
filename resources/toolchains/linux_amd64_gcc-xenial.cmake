26d3da8a93e46554ee9e8436f13457ef67d6e348
alex <alex@staticlibs.net>
2017-10-07 23:55:38 +0100

    ubuntu xenial support

---
+++ resources/toolchains/linux_amd64_gcc-xenial.cmake
@@ -0,0 +1,43 @@
+# Copyright 2017, alex at staticlibs.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cmake_minimum_required ( VERSION 2.8.12 )
+
+set ( CMAKE_BUILD_TYPE "Release" CACHE STRING "Default build type" )
+
+set ( CMAKE_SYSTEM_NAME Linux )
+set ( CMAKE_C_COMPILER gcc )
+set ( CMAKE_CXX_COMPILER g++ )
+set ( CMAKE_C_FLAGS "-fPIC" CACHE INTERNAL "" )
+set ( CMAKE_C_FLAGS_DEBUG "-g -O0" CACHE INTERNAL "" )
+set ( CMAKE_C_FLAGS_RELEASE "-Os -DNDEBUG" CACHE INTERNAL "" )
+set ( CMAKE_CXX_FLAGS "--std=c++14 -fPIC -Wall -Werror -Wextra" )
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-strict-overflow -fno-strict-aliasing -fstack-protector-all" )
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wlogical-op" )
+# log4cplus
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations" )
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE INTERNAL "" )
+set ( CMAKE_CXX_FLAGS_DEBUG "-g -O0" CACHE INTERNAL "" )
+set ( CMAKE_CXX_FLAGS_RELEASE "-Os -DNDEBUG" CACHE INTERNAL "" )
+
+# https://stackoverflow.com/a/6331749/314015
+set ( CMAKE_EXE_LINKER_FLAGS "-Wl,-rpath -Wl,$ORIGIN -Wl,-z -Wl,origin" CACHE INTERNAL "" )
+set ( CMAKE_SKIP_RPATH TRUE CACHE INTERNAL "" )
+
+# variables for packages that are present, but do not have pkg-config support
+set ( WILTON_PKGCONFIG_LIBDIR "-L/usr/lib/x86_64-linux-gnu/" CACHE INTERNAL "" )
+set ( WILTON_PKGCONFIG_INCLUDEDIR "-I/usr/include/ -I/usr/include/soci/ -I/usr/include/postgresql/" CACHE INTERNAL "" )
+set ( log4cplus_VERSION "1.1.2-3" CACHE INTERNAL "" )
+set ( asio_VERSION "1.10.6-3" CACHE INTERNAL "" )
+set ( soci_VERSION "3.2.3-1" CACHE INTERNAL "" )
