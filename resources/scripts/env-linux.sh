27f5827371157af92ea3bf8b2465d3578813df06
alex <alex@staticlibs.net>
2017-11-01 19:00:10 +0000

    android build wip

--- resources/scripts/env-linux.sh
+++ resources/scripts/env-linux.sh
@@ -2,9 +2,9 @@
 
 WILTON_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/../../
 
-PATH=$PATH:"$WILTON_DIR"/tools/linux/cmake/bin
-PATH=$PATH:"$WILTON_DIR"/tools/linux/jdk8/bin
-PATH=$PATH:"$WILTON_DIR"/deps/cmake/resources/creset
+export PATH=$PATH:"$WILTON_DIR"/tools/linux/cmake/bin
+export PATH=$PATH:"$WILTON_DIR"/tools/linux/jdk8/bin
+export PATH=$PATH:"$WILTON_DIR"/deps/cmake/resources/creset
 
-JAVA_HOME="$WILTON_DIR"/tools/linux/jdk8
-M2_HOME="$WILTON_DIR"/tools/maven
+export JAVA_HOME="$WILTON_DIR"/tools/linux/jdk8
+export M2_HOME="$WILTON_DIR"/tools/maven
