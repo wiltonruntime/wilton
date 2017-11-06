c8037cb053ef5557ff18b645a0aa605c0d3caf7c
alex <alex@staticlibs.net>
2017-11-06 09:35:30 -0800

    macosx build

---
+++ resources/toolchains/macosx_amd64_clang-mavericks.cmake
@@ -0,0 +1,40 @@
+# Copyright 2015, alex at staticlibs.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+cmake_minimum_required ( VERSION 2.8.12 )
+
+set ( CMAKE_BUILD_TYPE "Release" CACHE STRING "Default build type" )
+
+set ( CMAKE_SYSTEM_NAME Darwin )
+set ( CMAKE_C_COMPILER clang )
+set ( CMAKE_CXX_COMPILER )
+set ( CMAKE_C_FLAGS "-fPIC" CACHE INTERNAL "" )
+set ( CMAKE_C_FLAGS_DEBUG "-g -O0" CACHE INTERNAL "" )
+set ( CMAKE_C_FLAGS_RELEASE "-Os -DNDEBUG" CACHE INTERNAL "" )
+set ( CMAKE_CXX_FLAGS "--std=c++11 -fPIC -Wall -Wextra" )
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-strict-overflow -fno-strict-aliasing -fstack-protector-all" )
+# log4cplus
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations" )
+set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE INTERNAL "" )
+set ( CMAKE_CXX_FLAGS_DEBUG "-g -O0" CACHE INTERNAL "" )
+set ( CMAKE_CXX_FLAGS_RELEASE "-Os -DNDEBUG" CACHE INTERNAL "" )
+
+# https://stackoverflow.com/a/6331749/314015
+#set ( CMAKE_EXE_LINKER_FLAGS "-Wl,-rpath -Wl,$ORIGIN -Wl,-z -Wl,origin" CACHE INTERNAL "" )
+#set ( CMAKE_SKIP_RPATH TRUE CACHE INTERNAL "" )
+
+# variables for packages that are present, but do not have pkg-config support
+set ( WILTON_PKGCONFIG_LIBDIR "-L/usr/lib/" CACHE INTERNAL "" )
+set ( WILTON_PKGCONFIG_INCLUDEDIR "-I/usr/include/" CACHE INTERNAL "" )
+#set ( log4cplus_VERSION "1.1.2-3" CACHE INTERNAL "" )
