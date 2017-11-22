6dbc9cd2e19bff622693a8ef6faaacdfbc6d9106
alex <alex@staticlibs.net>
2017-11-22 12:19:13 +0000

    v201711221 docs

--- docs/html/search/functions_6c.js
+++ docs/html/search/functions_6c.js
@@ -6,6 +6,7 @@ var searchData=
   ['loadmodulejson',['loadModuleJson',['../namespaceloader.html#ae8ca04831c4de603c6abe7c4ab059e71',1,'loader']]],
   ['loadmoduleresource',['loadModuleResource',['../namespaceloader.html#ac3c61aedef71ee801035eef8d43e40a6',1,'loader']]],
   ['loadqueryfile',['loadQueryFile',['../namespaceDBConnection.html#ae8a22431181a6aa1965f13f9ada0aaa5',1,'DBConnection']]],
+  ['localecomparator',['localeComparator',['../namespaceutils.html#a5dc98f63e16fd34d9374f8b6accfb656',1,'utils']]],
   ['log',['log',['../namespaceLogger.html#a8ce5e4ce702911d4d66dcec8d64e3c02',1,'Logger']]],
   ['logger',['Logger',['../namespaceLogger.html#ae0d2e31ddd21695a14497123422d0a58',1,'Logger']]],
   ['lookup',['lookup',['../namespaceChannel.html#a079832a7e54fc437d467301ef40a9677',1,'Channel']]]
