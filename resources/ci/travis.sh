61e7a71445892f6b13dd983ca9ddcdda3a7bb0d4
alex <alex@staticlibs.net>
2018-05-18 19:37:09 +0100

    v8 and mt chakracore engines

--- resources/ci/travis.sh
+++ resources/ci/travis.sh
@@ -39,6 +39,8 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     # JSC is broken subtly on u1404
     # make test_js > test_js.log
     make test_duktape > test_duktape.log
+    make test_chakracore > test_chakracore.log
+    make test_v8 > test_v8.log
     make test_jvm > test_jvm.log
     make dist_linux_jre
     make test_rhino > test_rhino.log
