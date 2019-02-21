7bbc22b14e0840a17b4d545dd90957507237dc12
alex <alex@staticlibs.net>
2019-02-21 15:05:40 +0000

    v201902211 docs

--- docs/html/search/functions_66.js
+++ docs/html/search/functions_66.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['findmoduledirectory',['findModuleDirectory',['../namespaceloader.html#a47a280eee3d7e43158849d601974d4e8',1,'loader']]],
   ['findmodulepath',['findModulePath',['../namespaceloader.html#a1f1acda6a7e200c29eafabf49aa34bf2',1,'loader']]],
   ['form',['form',['../namespaceRequest.html#a0779eafbd4285cd2c3b1e97922ecf5f9',1,'Request']]],
   ['formaterror',['formatError',['../namespaceutils.html#ac0d45ff97ba10badeb7d5c0e600bdb9c',1,'utils']]],
