2a678963b015ba59367904badd56fd9b455cfff5
alex <alex@staticlibs.net>
2019-06-22 21:51:18 +0100

    v201906221 docs

--- docs/html/search/functions_70.js
+++ docs/html/search/functions_70.js
@@ -5,5 +5,6 @@ var searchData=
   ['pgconnection',['PGConnection',['../namespacePGConnection.html#a31a1aacbc4e55f2199e1a9adfefaf917',1,'PGConnection']]],
   ['poll',['poll',['../namespaceChannel.html#af3f31c5cce070639fe7b59e3597869bd',1,'Channel']]],
   ['prettify',['prettify',['../namespacehex.html#a1b200de92bf51458771ca63daa72c2be',1,'hex']]],
-  ['promisifyall',['promisifyAll',['../namespaceutils.html#a82a197e20f9fa0686b84e4f443996729',1,'utils']]]
+  ['promisifyall',['promisifyAll',['../namespaceutils.html#a82a197e20f9fa0686b84e4f443996729',1,'utils']]],
+  ['pull',['pull',['../namespacegit.html#ac2864fd2184115e4e7aa057b36c691a2',1,'git']]]
 ];
